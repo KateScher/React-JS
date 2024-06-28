9404cf62909d10c71e3f1f78846e94e0ca54a38e
Ekaterina Scherbakova <kat-m1@mail.ru>
2024-06-28 13:32:32 +0500

    Add part 2 of seminar 3

--- seminar_3/src/App.js
+++ seminar_3/src/App.js
@@ -1,15 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Greeting from "./components/greeting";
 import Counter from "./components/counter";
+import MessagesList from "./components/MessagesList";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+import TextDisplayForm from "./components/TextDisplayForm";
 
 function App() {
   const name = "Екатерина!";
   return (
     <div className="App">
       <h1>Изучаем React!</h1>
+      <TextDisplayForm />
       <Greeting name={name} />
       <Counter />
+      <MessagesList />
+      <ThemeSwitcher />
     </div>
   );
 }
